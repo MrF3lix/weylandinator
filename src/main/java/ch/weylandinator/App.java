7ee64dc9da4722997882f8708346c94155bd3c8a
Niklas van der Heide <vandenik@students.zhaw.ch>
2021-01-06 10:46:07 +0100

    Add logo

--- src/main/java/ch/weylandinator/App.java
+++ src/main/java/ch/weylandinator/App.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -16,6 +17,7 @@ public class App extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/Main.fxml"));
         stage.setTitle("Weylandinator");
         stage.setScene(new Scene(root));
+        stage.getIcons().add(new Image("logo.png"));
         stage.show();
     }
 
