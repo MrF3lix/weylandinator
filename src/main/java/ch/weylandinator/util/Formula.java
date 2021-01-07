384008ecf328631e9027cddd26effe1f90ea4b53
Little-Silver <pascal.isliker@hotmail.com>
2021-01-07 23:32:53 +0100

    Formula-Test and Calculator changes

--- src/main/java/ch/weylandinator/util/Formula.java
+++ src/main/java/ch/weylandinator/util/Formula.java
@@ -5,6 +5,8 @@ import static ch.weylandinator.util.Calculator.SIGMA_L;
 public enum Formula
 {
     URI("U = R * I"),
+    URI_2("R = U / I"),
+    URI_3("I = U / R"),
     PUI("P = U * I"),
     KNOT("I_in = I_out1 + I_out2"),
     MESH("0 = " + SIGMA_L + "[I_n]"),
@@ -24,6 +26,14 @@ public enum Formula
     
     public void replaceVariable(String variable, double value){
         content = content.replace(variable, String.valueOf(value));
+    }
+
+    public int numberOfVariables()
+    {
+        String variableString;
+        variableString = content.replaceAll("[^(a-z|A-Z|0-9|_)]"," ");
+        String[] variables = StringOperation.removeDuplicateSpaces(variableString).split(" ");
+        return variables.length;
     }
 
     public String getContent()
